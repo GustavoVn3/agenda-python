22bbcac269693bdbfbce4d66067ada0e4459329a
System Administrator <root@MacBook-Pro-de-Gustavo.local>
2021-11-26 13:12:58 -0300

    criacao de templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
 # Create your views here.
+def event(request):
+    tit = Evento.objects.all()
+    dados = {'eventos':tit}
+    return render(request, 'agenda.html', dados)
